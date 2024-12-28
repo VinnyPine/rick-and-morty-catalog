56d7cd5fcfa89045e47146644c936bf1b6b453ad
Vinicius Pinheiro <viniciusps_1998@hotmail.com>
2024-12-28 19:44:06 -0300

    fix: tests

--- src/app/pages/character/character.component.spec.ts
+++ src/app/pages/character/character.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CharacterComponent } from './character.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CharacterComponent', () => {
   let component: CharacterComponent;
@@ -8,9 +10,9 @@ describe('CharacterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CharacterComponent]
-    })
-    .compileComponents();
+      imports: [CharacterComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CharacterComponent);
     component = fixture.componentInstance;
--- src/app/pages/episode/episode.component.spec.ts
+++ src/app/pages/episode/episode.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EpisodeComponent } from './episode.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('EpisodeComponent', () => {
   let component: EpisodeComponent;
@@ -8,9 +10,9 @@ describe('EpisodeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EpisodeComponent]
-    })
-    .compileComponents();
+      imports: [EpisodeComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(EpisodeComponent);
     component = fixture.componentInstance;
